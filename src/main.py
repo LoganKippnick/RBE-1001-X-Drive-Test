69485433c91a654fd60f1d0ead59a55854b7d8cd
LoganKippnick <lgkippnick@outlook.com>
2024-02-07 09:54:13 -0500

    field oriented drive

--- src/main.py
+++ src/main.py
@@ -26,6 +26,10 @@ class DevicePorts:
 class Drive:
     class Constants:
         WHEEL_DIAMETER_IN = 4
+        WHEEL_CIRCUMFERENCE_IN = WHEEL_DIAMETER_IN * math.pi
+
+        DRIVE_BASE_RADIUS_IN = 7.75 # TODO
+        DRIVE_BASE_CIRCUMFERENCE_IN = DRIVE_BASE_RADIUS_IN * 2 * math.pi
 
         MOTOR_MAX_SPEED_RPM = 200
 
@@ -37,11 +41,18 @@ class Drive:
     gyro = Inertial(DevicePorts.GYRO)
 
     def __init__(self):
-        pass
-        
+        self.gyro.calibrate()
+        while self.gyro.is_calibrating():
+            pass
+        print("GYRO CALIBRATED")
+
     @staticmethod
     def __metersPerSecToRPM(speedMetersPerSec):
-        return (speedMetersPerSec * 39.3701 * 60) / (Drive.Constants.WHEEL_DIAMETER_IN * math.pi)
+        return (speedMetersPerSec * 39.3701 * 60) / (Drive.Constants.WHEEL_CIRCUMFERENCE_IN)
+    
+    @staticmethod
+    def __radPerSecToRPM(speedRadPerSec):
+        return (speedRadPerSec * Drive.Constants.DRIVE_BASE_CIRCUMFERENCE_IN * 60) / (2 * math.pi * Drive.Constants.WHEEL_CIRCUMFERENCE_IN)
     
     def applyDesaturated(self, flSpeedRPM, frSpeedRPM, blSpeedRPM, brSpeedRPM):
         fastestSpeedRPM = max(abs(flSpeedRPM), abs(frSpeedRPM), abs(blSpeedRPM), abs(brSpeedRPM))
@@ -57,30 +68,35 @@ class Drive:
         self.brDrive.spin(FORWARD, brSpeedRPM)
 
     SEC_PHI = 2 / math.sqrt(2)
-    def translate(self, directionRad: float, speedMetersPerSec: float):
-        rpm = self.__metersPerSecToRPM(speedMetersPerSec)
+    def applySpeeds(self, directionRad: float, translationSpeedMetersPerSec: float, rotationSpeedRadPerSec: float, fieldOriented: bool):
+        translationRPM = self.__metersPerSecToRPM(translationSpeedMetersPerSec)
+        rotationRPM = -self.__radPerSecToRPM(rotationSpeedRadPerSec)
 
-        coeff = self.SEC_PHI * rpm
+        if fieldOriented:
+            directionRad -= self.gyro.heading(RotationUnits.REV) * 2 * math.pi
+
+        coeff = self.SEC_PHI * translationRPM
         xProjection = coeff * math.sin(directionRad)
         yProjection = coeff * math.cos(directionRad)
 
         # zeroes will be rotation speed
         self.applyDesaturated(
-            0 - (xProjection - yProjection),
-            0 - (xProjection + yProjection),
-            0 + (xProjection + yProjection),
-            0 + (xProjection - yProjection)
+            rotationRPM - (xProjection - yProjection),
+            rotationRPM - (xProjection + yProjection),
+            rotationRPM + (xProjection + yProjection),
+            rotationRPM + (xProjection - yProjection)
         )
 
 drive = Drive()
 
 while True:
-    drive.translate(math.pi / 8, 0.1)
+    # drive.applySpeeds(math.pi / 8, 0.1, 0)
 
-    # xIn = controller.axis4.position() * 0.01
-    # yIn = controller.axis3.position() * 0.01
+    xIn = -controller.axis4.position() * 0.01
+    yIn = controller.axis3.position() * 0.01
+    thetaIn = -controller.axis1.position() * 0.01
 
-    # direction = math.atan2(xIn, yIn)
-    # # magnitude = math.hypot(xIn, yIn)
+    direction = math.atan2(xIn, yIn)
+    magnitude = math.sqrt(xIn**2 + yIn**2)
 
-    # drive.translate(direction, 100)
+    drive.applySpeeds(direction, magnitude * 1.0, thetaIn * 2 * math.pi, True)
